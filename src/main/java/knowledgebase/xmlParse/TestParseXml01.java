2afdf8c6b70ea3017817ebf27d5756b28c123683
dudong <pirate_dd@126.com>
2016-08-17 07:41:19 +0800

    添加了 dom4j.jar

---
+++ src/main/java/knowledgebase/xmlParse/TestParseXml01.java
@@ -0,0 +1,15 @@
+package knowledgebase.xmlParse;
+
+import org.dom4j.io.SAXReader;
+
+/**
+ * Created by Du on 16/8/16.
+ */
+public class TestParseXml01 {
+
+    public static void main(String[] args) {
+
+        SAXReader reader = new SAXReader();
+    }
+
+}
